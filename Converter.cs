ad625402d9c4fdd687ebf6e9343581674a9e151f
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-23 18:08:12 -0300

    Adicionar arquivos de projeto.

---
+++ Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex5
+{
+    public class Converter
+    {
+        public List<double> convertor(List<string> listaS) 
+        {
+            List<double> listaI = new List<double>();
+
+            listaI= listaS.ConvertAll(double.Parse);
+
+            return listaI;
+        }
+        public double logica(List<double> listaD) 
+        {
+            double resultado1 = (listaD[0] + listaD[1] + listaD[2]) / 2.0;
+            double area = Math.Sqrt(resultado1 * (resultado1 - listaD[0]) * (resultado1 - listaD[1]) * (resultado1 - listaD[2]));
+
+            return area;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program() 
+        {
+            Converter Conversor = new Converter();
+            List<string> listaS = new List<string>();
+            List<double> listaI = new List<double>();
+            double resulfinal;
+
+            for(int i = 0; i < 3; i++) 
+            {
+                Console.Write("Digite o valor 0{0} do triângulo: ", i+1);
+                listaS.Add(Console.ReadLine());
+            } 
+            listaI = Conversor.convertor(listaS);
+            resulfinal = Conversor.logica(listaI);
+
+            Console.WriteLine("O resultado final foi: {0} ", resulfinal);
+            Console.ReadKey();
+        }
+    }
+}
